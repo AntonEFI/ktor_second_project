cf36887fe955378c181098b4a547459634268bb9
AntonEFI <109530075+AntonEFI@users.noreply.github.com>
2026-06-01 16:16:34 +0500

    first_commit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,18 @@
 package com.example
 
-import io.ktor.server.engine.*
 import io.ktor.server.application.*
+import io.ktor.server.engine.*
 
 fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
+    embeddedServer(
+        factory = io.ktor.server.netty.Netty,
+        port = 9191,
+        host = "0.0.0.0",
+        module = Application::rootModule
+    ).start(wait = true)
+}
+
+
+fun Application.rootModule() {//Чтобы IDE успокоилось
+    // Сюда вы будете добавлять routing { ... } и другие настройки
 }
